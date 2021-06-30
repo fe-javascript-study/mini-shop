47d3cc2e13b0da9d153adc9ecb1bbf16f45aade2
chunyong <fr_lovemy5851>
2021-06-30 22:23:45 +0900

    fix : 미니샵 리스트 추가

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 import { getShop } from "./api/index";
 
-const getShopData = getShop().then((data) => console.log(data))
 
+const renderTotalList = () => {
+    const contents = document.querySelector('.shop--contents');
+
+    let list = '';
+
+    getShop().then((data) => {
+        data.map((item, idx) => contents.innerHTML += `<li data-idx="${idx}">
+            <span>${item.name}</span>
+            <span>${item.gender}</span>
+            <span>${item.size}</span>
+            <span>${item.color}</span>
+            <span>${item.price}</span>
+            <span>
+                <button>수정</button>
+                <button>삭제</button>
+            </span>
+        </li>`)
+    })
+}
+
+renderTotalList()
