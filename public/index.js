5c6ecf24f2e5347adb5f875c271c33d7f3ddbc05
GoDoyeon <97godo@naver.com>
2021-07-08 17:41:15 +0900

    [fix] axios 에러

--- public/index.js
+++ public/index.js
@@ -1,37 +1,27 @@
-// import axios from "axios";
+import axios from 'axios';
 
-// 상품 추가 모달 창
-const openItemAdd = document.getElementById('openItemAddBtn');
-const modal = document.querySelector('.modal');
-const overlay = modal.querySelector('.modal__btn');
-const closeBtn = modal.querySelector('.modal__btn');
-
-// 모달 창 열기
-const openModal = () => {
-  console.log('click');
-  modal.classList.remove('hidden');
-};
-// 모달 창 닫기
-const closeModal = () => {
-  modal.classList.add('hidden');
-};
-overlay.addEventListener('click', closeModal);
-closeBtn.addEventListener('click', closeModal);
-openItemAdd.addEventListener('click', openModal);
-
-/*
 const instance = axios.create({
- baseURL: 'http://localhost:8080'
+  baseURL: 'http://localhost:8000',
 });
 
 instance.interceptors.request.use(
-    function (config) {
-     config.headers["Content-Type"] = "application/json; charset=utf-8";
-     return config;
-    },
-    function (error) {
-     console.warn(error);
-     return Promise.reject(error);
-    }
+  function (config) {
+    config.headers['Content-Type'] = 'application/json; charset=utf-8';
+    console.log('dd');
+    return config;
+  },
+  function (error) {
+    console.warn(error);
+    return Promise.reject(error);
+  },
 );
-*/
+
+instance.interceptors.response.use(
+  function (response) {
+    return response.data;
+  },
+  function (error) {
+    console.warn(error);
+    return Promise.reject(error);
+  },
+);
